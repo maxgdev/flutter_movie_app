ff5f8b2a7483cd50f348f888e6d8eb5122fba5f5
maxgdev <max@maxgdev.net>
2021-02-24 13:55:39 +0000

    Added CircleAvatar and and subtitle to ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List movies = [
+    "The Shawshank Redemption",
+    "The Godfather",
+    "The Dark Knight",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "Fight Club",
+    "Inception",
+    "The Matrix",
+    "Seven Samurai ",
+    "City of God",
+    "Leon",
+    "Avengers: Infinity War",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,13 +49,26 @@ class _MyHomePageState extends State<MyHomePage> {
             backgroundColor: Colors.blueGrey.shade900),
         // background color for body/rest of app
         backgroundColor: Colors.blueGrey.shade400,
-        body: ListView.builder(itemBuilder: (BuildContext context, int index) {
-          return Card(
-            color: Colors.white,
-            child: ListTile(
-              title: Text("Movie title"),
-            ),
-          );
-        }));
+        body: ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                elevation: 5,
+                color: Colors.white,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(14)
+                      ),
+                      child: Text("$index"),
+                    ),
+                  ),
+                  title: Text(movies[index]),
+                  subtitle: Text('subtitle'),
+                ),
+              );
+            }));
   }
 }
