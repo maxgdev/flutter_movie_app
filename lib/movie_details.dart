7a40b0127b78013af3b699abe3e642340c777cbd
maxgdev <max@maxgdev.net>
2021-02-25 15:16:07 +0000

    Added MovieDetailsPoster & MoviePoster

--- lib/movie_details.dart
+++ lib/movie_details.dart
@@ -17,7 +17,10 @@ class MovieDetails extends StatelessWidget {
 
       body: ListView(
         children: [
-          MovieDetailsThumbnail(thumbnail: movie.images[0],)
+          MovieDetailsThumbnail(
+            thumbnail: movie.images[0],
+          ),
+          MovieDetailsPoster(movie: movie,)
         ],
       ),
       // body: Column(
@@ -54,31 +57,69 @@ class MovieDetailsThumbnail extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               height: 190,
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(thumbnail),
-                  fit: BoxFit.cover
-                )
-              ),
+                  image: DecorationImage(
+                      image: NetworkImage(thumbnail), fit: BoxFit.cover)),
             ),
             Icon(
-              Icons.play_circle_fill_outlined, 
+              Icons.play_circle_fill_outlined,
               size: 100,
               color: Colors.white,
             ),
-            
           ],
-          ),
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(            
-                colors: [Color(0x00f5f5f5), Color(0xfff5f5f5)],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter
-                )
-            ),
-            height: 80,
-          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0x00f5f5f5), Color(0xfff5f5f5)],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter)),
+          height: 80,
+        ),
       ],
+    );
+  }
+}
+
+class MovieDetailsPoster extends StatelessWidget {
+  final Movie movie;
+
+  const MovieDetailsPoster({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        children: [
+          MoviePoster(poster: movie.images[1].toString(),)
+        ],
+      ),
+    );
+  }
+}
+
+class MoviePoster extends StatelessWidget {
+  final String poster;
+
+  const MoviePoster({Key key, this.poster}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var borderRadius = BorderRadius.all(Radius.circular(10));
+    return Card(
+      child: ClipRRect(
+        borderRadius: borderRadius,
+        child: Container(
+          width: MediaQuery.of(context).size.width / 4,
+          height: 160,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: NetworkImage(poster),
+            fit: BoxFit.cover,
+          )
+          
+          ),
+        ),
+      ),
     );
   }
 }
